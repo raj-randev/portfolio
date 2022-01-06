f5a4cec0e01c0fe96708adf7148759df01e21530
Rajan Randev <randevr85@gmail.com>
2022-01-06 15:33:32 +0000

    Update FoxAnimate.js

--- src/components/projects-3d/FoxAnimate.js
+++ src/components/projects-3d/FoxAnimate.js
@@ -1,7 +1,7 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-import { Canvas, extend, useThree, useFrame } from '@react-three/fiber';
+import { Canvas, extend, useThree, useFrame, useLoader } from '@react-three/fiber';
 import * as THREE from 'three';
 import { useControls, folder, Leva } from 'leva';
 import Animation3DArray from '../database/Animation3DArray';
@@ -9,40 +9,18 @@ import Info from '../parts/Info';
 
 extend({OrbitControls});
 
-const FoxBody = () => {
-  
-    const { 
 
-        foxScale, 
-        foxPositionX,
-        foxPositionZ
-  
-    } = useControls('Control Panel',{ 
-  
-        Fox: folder({
+const Model = () => {
 
-            foxPositionX: {value: 0, min: -200, max: 200, step: 1, label: 'Position X'},
-            foxPositionZ: {value: 0, min: -200, max: 200, step: 1, label: 'Position Z'},
-            foxScale: {value: 0.02, min: 0.01, max: 2, step: 0.01, label: 'Scale'}
-
-        })
-        
-    })  
     
-    const [model, setModel] = useState();
-    
-    useEffect(() => {
 
-        new GLTFLoader().load('https://www.raj-ran.dev/models/Fox/glTF/Fox.gltf', setModel)
-
-    }, [setModel]);
-        
-    return model ? <primitive 
-        object={model.scene} 
-        position={[foxPositionX, 0, foxPositionZ]} 
-        scale={foxScale} 
-        /> : null
+    const gltf = useLoader(GLTFLoader, "/models/Fox/glTF/Fox.gltf")
     
+    return (
+        <>
+            <primitive  position={[0, 0, 0]} object={gltf.scene} scale={0.02} />
+        </>
+    );
 }
 
 const Controls = () => {
@@ -120,8 +98,6 @@ const FoxAnimate = () => {
                     gl.shadowMap.enabled = true
                     gl.shadowMap.type = THREE.PCFShadowMap
                 }}
-                width='100%'
-                height='100%'
             >
 
                 <ambientLight />
@@ -134,7 +110,7 @@ const FoxAnimate = () => {
 
                 <Controls />
 
-                <FoxBody />
+                <Model />
 
                 <Plane />
                 
